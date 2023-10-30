fc281c033d27fd027475f9aa623ab0eaa978cbcc
wwwccttoo <ketong_shao@berkeley.edu>
2023-10-29 23:52:34 -0400

    QE

---
+++ QE/Fe_110_paper_480c_ESM_pn5/Fe_NH/Fe_NH_paper_480c_ESM_pn5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=Fe_NH_paper_480c_ESM_pn5
+#SBATCH --account=fc_mllam
+#SBATCH --partition=savio
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=20
+#SBATCH --cpus-per-task=1
+#SBATCH --time=72:00:00
+#SBATCH --output=Fe_NH_paper_480c_ESM_pn5_%j.out
+#SBATCH --error=Fe_NH_paper_480c_ESM_pn5_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ketong_shao@berkeley.edu,jwang44@nd.edu
+## Command(s) to run:
+
+module load quantumespresso/6.4.1-qmcpack
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+mpirun pw.x -npool 8 < Fe_NH_paper_480c_ESM_pn5.pwi > Fe_NH_paper_480c_ESM_pn5.out
