c65c5a55496249ab6a4b1af1e79eaeb4a1fc6623
wwwccttoo <ketong_shao@berkeley.edu>
2023-10-29 23:53:38 -0400

    QE

---
+++ QE/Fe_110_paper_480c_ESM_VA4_ph/NH3/NH3_480c_ESM_VA4_ph.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=NH3_480c_ESM_p25_ph
+#SBATCH --account=ucb316
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=120
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=240000M 
+#SBATCH -t 48:00:00
+#SBATCH --output=NH3_480c_ESM_p25_ph_%j.out
+#SBATCH --error=NH3_480c_ESM_p25_ph_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=ketong_shao@berkeley.edu,jwang44@nd.edu
+## Command(s) to run:
+
+export SLURM_EXPORT_ENV=ALL
+module purge
+module load cpu/0.15.4
+module load gcc/9.2.0
+module load openmpi/3.1.6
+module load quantum-espresso/6.7.0-openblas
+module load slurm
+
+srun -n 120 ph.x -npool 8 < NH3_480c_ESM_p25_ph.phi > NH3_480c_ESM_p25_ph.out
